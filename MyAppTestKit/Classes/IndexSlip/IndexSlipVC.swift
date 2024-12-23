d2971de94e823695df229841f1bc34e322ec4d53
zdh-123 <2746315561@qq.com>
2024-12-23 17:08:12 +0800

    添加一个索引条页面，添加SnapKit依赖

---
+++ MyAppTestKit/Classes/IndexSlip/IndexSlipVC.swift
@@ -0,0 +1,21 @@
+//
+//  IndexSlipVC.swift
+//  MyAppTestKit
+//
+//  Created by zdh on 2024/12/23.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+/// 索引条页面，该页面用来验证复用机制使用的，使用到UItableview
+class IndexSlipVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "索引条页面"
+    }
+    
+    
+}
