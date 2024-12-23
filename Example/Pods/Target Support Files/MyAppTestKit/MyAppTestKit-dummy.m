9f56da088535b7bcd443098fbecf494372038357
zdh-123 <2746315561@qq.com>
2024-12-23 16:01:56 +0800

    第一次提交

---
+++ Example/Pods/Target Support Files/MyAppTestKit/MyAppTestKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyAppTestKit : NSObject
+@end
+@implementation PodsDummy_MyAppTestKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyAppTestKit_Example/Pods-MyAppTestKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyAppTestKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MyAppTestKit_Example
+@end
